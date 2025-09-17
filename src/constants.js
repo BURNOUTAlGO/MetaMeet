622afc6c5c2d2c497a565dfdee94722c8cfa829b
thisi <thisiabhimaurya@gmail.com>
2025-09-17 13:02:22 +0530

    second_key_integerate

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
- export const  APP_ID =4583508;
- export const SERVER_SECRET='d1f472262b0299d942ad37203077baf5';
+ export const  APP_ID =1101322625;
+ export const SERVER_SECRET='5b29e956b69d3ea0324cb8a51e4ad55d';
 
